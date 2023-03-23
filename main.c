59da3740393fff561aeb65a69a34281879ba96cd
kimaita <johnnieqaym@gmail.com>
2023-03-23 23:44:14 +0300

    edits

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
 	/* check for proper number of arguments */
 	if (argc != 2)
 	{
-		fprintf(stdout, "USAGE: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
@@ -30,7 +30,7 @@ int main(int argc, char **argv)
 	file_in = fopen(argv[1], "r");
 	if (file_in == NULL)
 	{
-		fprintf(stdout, "Error: Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 
@@ -53,7 +53,7 @@ int main(int argc, char **argv)
 			instruction->f(&top, line_number);
 		else
 		{
-			fprintf(stdout, "L%d: unknown instruction %s\n",
+			fprintf(stderr, "L%d: unknown instruction %s\n",
 				line_number, instruction->opcode);
 			if (line)
 				free(line);
